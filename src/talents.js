ef06280251bfb939a79a3741735b8847242f65f6
PetitPrince <code@hoangpham.ch>
2022-09-11 21:51:33 +0200

    Overfly complicated 1st version

---
+++ src/talents.js
@@ -0,0 +1,354 @@
+export const talents_par_defaut = [
+    {
+      "specialisation": "Générique",
+      "nom": "Acrobatie",
+      "caracteristique_associe": "Agilité",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Aisance sociale",
+      "caracteristique_associe": "Présence",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Art",
+      "caracteristique_associe": "Présence",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Athlétisme",
+      "caracteristique_associe": "Force",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Baratin",
+      "caracteristique_associe": "Présence",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Combat",
+      "caracteristique_associe": "Agilité",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Conduite",
+      "caracteristique_associe": "Agilité",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Contorsionisme",
+      "caracteristique_associe": "Agilité",
+      "inne": false,
+      "superieur_exotique": "Christophe, Janus, Kobal, Valefor",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Corps à corps",
+      "caracteristique_associe": "Agilité",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Culture générale",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Défense",
+      "caracteristique_associe": "Agilité",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Discrétion",
+      "caracteristique_associe": "Agilité",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Discussion",
+      "caracteristique_associe": "Volonté",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Enquête",
+      "caracteristique_associe": "Chance / Foi",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Fouille",
+      "caracteristique_associe": "Perception",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Multiple",
+      "nom": "Hobby",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Humour",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "Christophe, Haagenti, Kobal",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Hypnotisme",
+      "caracteristique_associe": "Volonté",
+      "inne": false,
+      "superieur_exotique": "Joseph, Walther, Abalam, Nog, Scox",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Informatique",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Intimidation",
+      "caracteristique_associe": "Force",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Intrusion",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Jeu",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "Asmodée",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Kama sutra",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "Andrealphus",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Language animal",
+      "caracteristique_associe": "Perception",
+      "inne": false,
+      "superieur_exotique": "Jordi, Novalis, Caym",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Multiple",
+      "nom": "Langues",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Médecine",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Multiple",
+      "nom": "Métier",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Narcolepsie",
+      "caracteristique_associe": "Volonté",
+      "inne": false,
+      "superieur_exotique": "Blandine, Beleth, Nog",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Navigation",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Pickpocket",
+      "caracteristique_associe": "Agilité",
+      "inne": false,
+      "superieur_exotique": "Janus, Valefor",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Pilotage",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Prestedigitation",
+      "caracteristique_associe": "Agilité",
+      "inne": false,
+      "superieur_exotique": "Christophe, Janus, Valefor",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Savoir criminel",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Savoir d’espion",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Savoir militaire",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Savoir occulte",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Science",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Séduction",
+      "caracteristique_associe": "Présence",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Sixième sens",
+      "caracteristique_associe": "Chance / Foi",
+      "inne": false,
+      "superieur_exotique": "Dominique, Andromalius",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Survie",
+      "caracteristique_associe": "Perception",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Technique",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "",
+      "type": "Secondaire"
+    },
+    {
+      "specialisation": "Spécifique",
+      "nom": "Tir",
+      "caracteristique_associe": "Perception",
+      "inne": true,
+      "superieur_exotique": "",
+      "type": "Principal"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Torture",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "Joseph",
+      "type": "Exotique"
+    },
+    {
+      "specialisation": "Générique",
+      "nom": "Ventriloquie",
+      "caracteristique_associe": "Aucune",
+      "inne": false,
+      "superieur_exotique": "Christophe",
+      "type": "Exotique"
+    }
+  ];

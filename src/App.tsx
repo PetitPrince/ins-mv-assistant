fac17dfde5f4a6dd5707bb12c8ef26781e2977a8
PetitPrince <code@hoangpham.ch>
2022-09-15 00:11:43 +0200

    Some kind of billing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { MantineProvider, NumberInputProps } from '@mantine/core';
 import { Button, TextInput, NumberInput, Autocomplete, Stack, Group, Select, Title, Table } from '@mantine/core';
-import { Radio, Grid, ActionIcon, Dialog,Text } from '@mantine/core';
+import { Radio, Grid, ActionIcon, Dialog, Text } from '@mantine/core';
 import { talents_par_defaut } from './talents';
 import { IconPlus, IconMinus, IconRotate, IconCheck, IconX } from '@tabler/icons'
 import React, { Component, useEffect } from 'react';
@@ -21,8 +21,8 @@ const INSMVNumberInput = (props: any) => {
 
 const INSMVCaraNumberInput = (props: any) => {
 
-  const {computeCaracCost,initialValue, ...restOfTheProps} = props; // extracting computeCaracCost and initialValue from props (I want to pass props forward to NumberInput)
-  const coutPourUneUpgradeDeForce = computeCaracCost(props.value,initialValue);
+  const { computeCaracCost, initialValue, ...restOfTheProps } = props; // extracting computeCaracCost and initialValue from props (I want to pass props forward to NumberInput)
+  const coutPourUneUpgradeDeForce = computeCaracCost(props.value, initialValue);
   const isTooUpgradeExpenseive = coutPourUneUpgradeDeForce > props.pa;
   const variant = props.value === initialValue ? "default" : "filled";
   const errMsg = isTooUpgradeExpenseive ? "Pas assez de PA" : "";
@@ -96,11 +96,11 @@ function Blessures(props: { force: number; faction: string; }) {
   );
 }
 
-function Status2(props:any){
-  const [state, setState] = useSetState({pa: props.pa, paTotal: props.paTotal});
+function Status2(props: any) {
+  const [state, setState] = useSetState({ pa: props.pa, paTotal: props.paTotal });
 
-  const statusChangeHandler = (val: number, field:string) => {
-    const updatedState : Partial<IAssistantState> = {[field]: val};
+  const statusChangeHandler = (val: number, field: string) => {
+    const updatedState: Partial<IAssistantState> = { [field]: val };
     setState(updatedState);
     props.statusChangeHandler(updatedState);
   };
@@ -109,7 +109,7 @@ function Status2(props:any){
     <Stack>
       <Title order={2}>Status</Title>
       <Group>
-        <NumberInput label="Point d'Administration (PA)" value={state.pa} onChange={(val)=>{statusChangeHandler(val||0, "pa")}} />
+        <NumberInput label="Point d'Administration (PA)" value={state.pa} onChange={(val) => { statusChangeHandler(val || 0, "pa") }} />
         {/* <NumberInput label="PA accumulés" value={state.paTotal} onChange={(val)=>{statusChangeHandler(val||0, "paTotal")}} /> */}
         {/* <NumberInput label="Point de Pouvoir (PP)" value={this.props.pp} onChange={this.props.handlePpChange} />
         <NumberInput label="PP Maximum" value={this.props.ppMax} onChange={this.props.handlePpMaxChange} />
@@ -119,11 +119,11 @@ function Status2(props:any){
     </Stack>
   );
 }
-function computeCaracCost(finalValue:number, initialValue:number){
-  if(finalValue){
-    const diff = finalValue-initialValue;
-    return (diff)*4;  
-  }else{
+function computeCaracCost(finalValue: number, initialValue: number) {
+  if (finalValue) {
+    const diff = finalValue - initialValue;
+    return (diff) * 4;
+  } else {
     return 0;
   }
 }
@@ -132,9 +132,9 @@ type TCaraFuncProps = {
   caracteristiques: TcaracteristiquesSet;
   initialcaracteristiques: TcaracteristiquesSet;
   pa: number;
-  onChangeCara: (x:any)=>void;
+  onChangeCara: (x: any) => void;
 }
-function Caracteristiques(props:TCaraFuncProps){
+function Caracteristiques(props: TCaraFuncProps) {
   const [state, setState] = useSetState({
     force: props.caracteristiques.force,
     agilite: props.caracteristiques.agilite,
@@ -142,42 +142,54 @@ function Caracteristiques(props:TCaraFuncProps){
     presence: props.caracteristiques.presence,
     foi: props.caracteristiques.foi,
   });
-  const onChangeCara = (val:number, cara:string) => {
-    setState({[cara]: val});
+  const onChangeCara = (val: number, cara: string) => {
+    setState({ [cara]: val });
 
     const updateMap = {
       caracteristiques: {
         [cara]: val
       },
-      caraBillingItem:{
-        [cara]:{
-          msg: [cara]+": "+props.caracteristiques[cara]+"->"+val,
-          cost: computeCaracCost(val,props.caracteristiques[cara])
+      caraBillingItem: {
+        ["cara_"+cara]: {
+          msg: [cara] + ": " + props.initialcaracteristiques[cara] + "->" + val,
+          cost: computeCaracCost(val, props.initialcaracteristiques[cara])
         }
       }
     }
     props.onChangeCara(updateMap);
   }
 
-    return (
-      <Stack>
-        <Title order={2}>Caractéristiques</Title>
-        <Group>
-          <INSMVCaraNumberInput label="Force" value={state.force} initialValue={props.initialcaracteristiques.force} onChange={(val: number)=>{onChangeCara(val, "force")}} computeCaracCost={computeCaracCost} pa={props.pa} />
-          <INSMVCaraNumberInput label="Agilité" value={state.agilite} initialValue={props.initialcaracteristiques.agilite} onChange={(val: number)=>{onChangeCara(val, "agilite")}} computeCaracCost={computeCaracCost} pa={props.pa} />
-          <INSMVCaraNumberInput label="Perception" value={state.perception} initialValue={props.initialcaracteristiques.perception} onChange={(val: number)=>{onChangeCara(val, "perception")}} computeCaracCost={computeCaracCost} pa={props.pa} />
-          <INSMVCaraNumberInput label="Présence" value={state.presence} initialValue={props.initialcaracteristiques.presence} onChange={(val: number)=>{onChangeCara(val, "presence")}} computeCaracCost={computeCaracCost} pa={props.pa} />
-          <INSMVCaraNumberInput label="Foi" value={state.foi} initialValue={props.initialcaracteristiques.foi} onChange={(val: number)=>{onChangeCara(val, "foi")}} computeCaracCost={computeCaracCost} pa={props.pa} />
-        </Group>
+  return (
+    <Stack>
+      <Title order={2}>Caractéristiques</Title>
+      <Group>
+        <INSMVCaraNumberInput label="Force" value={state.force} initialValue={props.initialcaracteristiques.force} onChange={(val: number) => { onChangeCara(val, "force") }} computeCaracCost={computeCaracCost} pa={props.pa} />
+        <INSMVCaraNumberInput label="Agilité" value={state.agilite} initialValue={props.initialcaracteristiques.agilite} onChange={(val: number) => { onChangeCara(val, "agilite") }} computeCaracCost={computeCaracCost} pa={props.pa} />
+        <INSMVCaraNumberInput label="Perception" value={state.perception} initialValue={props.initialcaracteristiques.perception} onChange={(val: number) => { onChangeCara(val, "perception") }} computeCaracCost={computeCaracCost} pa={props.pa} />
+        <INSMVCaraNumberInput label="Présence" value={state.presence} initialValue={props.initialcaracteristiques.presence} onChange={(val: number) => { onChangeCara(val, "presence") }} computeCaracCost={computeCaracCost} pa={props.pa} />
+        <INSMVCaraNumberInput label="Foi" value={state.foi} initialValue={props.initialcaracteristiques.foi} onChange={(val: number) => { onChangeCara(val, "foi") }} computeCaracCost={computeCaracCost} pa={props.pa} />
+      </Group>
 
-        {/* <Text>debug force: {props.force}; debug agilite {props.agilite}</Text> */}
-      </Stack>
-  
-   ); 
+      {/* <Text>debug force: {props.force}; debug agilite {props.agilite}</Text> */}
+    </Stack>
+
+  );
 
 }
 
-function PersistentPanel(props:any) {
+
+function BillingPanel(props: 
+  { billingState: { [s: string]: IBillingItem; }}) {
+
+  console.log(props);
+  let sum = 0;
+
+  for (const billingItem of Object.values(props.billingState)) {
+    sum+= billingItem.cost;
+    
+  }
+
+  
 
   // maybe use Dialog instead
   return <Dialog opened={true} position={{ top: 20, right: 20 }}>
@@ -189,31 +201,28 @@ function PersistentPanel(props:any) {
           <th>Action</th>
         </tr>
       </thead>
-      <tbody>
-        {/* {
-        props.billingItems.map((billingItem:any)=>{
-          <tr>
-            <td>{billingItem.cost}</td>
-            <td>{billingItem.msg}</td>
+      <tbody> 
+        {
+          Object.values(props.billingState)
+            .map((billingItem: any) => {
+              if (Object.keys(billingItem).length){
+                return (
+                  <tr>
+                    <td>{billingItem.cost}</td>
+                    <td>{billingItem.msg}</td>
+                  </tr>
+  
+                );
+              }else{
+                return;
+              }
+            })
 
-          </tr>
-        })
-        
-        } */}
+        }
+
+
         <tr>
-          <td>1337</td>
-          <td> initial</td>
-        </tr>
-        <tr>
-          <td>-4</td>
-          <td> caracteristiquess</td>
-          <td>
-            <IconCheck size={12} />
-            <IconX size={12} />
-          </td>
-        </tr>
-        <tr>
-          <td>24</td>
+          <td>{sum}</td>
           <td> total</td>
           <td>
             <IconCheck size={12} />
@@ -228,7 +237,7 @@ function PersistentPanel(props:any) {
 
 
 type TcaracteristiquesSet = {
-  [K in "force"|"agilite"|"perception"|"volonte"|"presence"|"foi" as string] : number;
+  [K in "force" | "agilite" | "perception" | "volonte" | "presence" | "foi" as string]: number;
   // force: number;
   // agilite: number;
   // perception: number;
@@ -237,83 +246,78 @@ type TcaracteristiquesSet = {
   // foi: number;  
 }
 
-interface IPerso{
+interface IPerso {
   identite: string;
   faction: string;
   superieur: string;
   grade: number;
-  caracteristiques : TcaracteristiquesSet;
+  caracteristiques: TcaracteristiquesSet;
   pa: number;
   paTotal: number;
   pp: number;
-  ppMax: number;  
+  ppMax: number;
 
 }
 interface IAssistantProps {
   perso: IPerso;
 }
-interface IAssistantState extends IPerso{
+interface IAssistantState extends IPerso {
   billingState?: {
     caracteristiquess?: IBillingItem[],
     talents?: IBillingItem[],
-    pouvoirs?:IBillingItem[]
+    pouvoirs?: IBillingItem[]
   };
 
 }
-interface IBillingItem{
+interface IBillingItem {
   msg: string,
   cost: number
 }
 
-class AssistantFunc(props){
-  
-}
-  class Assistant3 extends React.Component<IAssistantProps, IAssistantState> {
+function FeuilleDePerso(props: any) {
+  const [state, setState] = useSetState({
+    ...props.perso,
+    billingState: {
+    }
+  });
 
-  onChangeCaraAndBilling: (updateMap:  {[key: string]: any}) => void;
-  statusChangeHandler: (updatedState: Pick<IAssistantState, keyof IAssistantState>) => void; // initially used Partial<IAssistantState>, but we cannot use this, see https://stackoverflow.com/questions/59737459/react-typescript-how-to-update-state-with-single-object-with-a-dynamic-set
-  constructor(props: IAssistantProps) {
-    super(props);
-    this.state = {...props.perso,
-    billingState: {}
+  const statusChangeHandler = (updatedState: Pick<IAssistantState, keyof IAssistantState>) => {
+    setState(updatedState);
+  }
+  const caraChangeHandler = (updateMap: {caracteristiques: Partial<TcaracteristiquesSet>; caraBillingItem: IBillingItem; }) => {
+    console.log(updateMap);
+    let updatedBillingState = {
+      ...state.billingState,
+      ...updateMap.caraBillingItem
+    }
+    setState({ billingState: updatedBillingState })
+
+    setState({
+      caracteristiques: {
+        ...state.caracteristiques,
+        ...updateMap.caracteristiques
+      }
+    })
   };
 
-    this.onChangeCaraAndBilling = (updateMap) => {
-      console.log(updateMap);
+  return (
+    <Stack>
 
-      this.setState({
-        caracteristiques:{
-          ...this.state.caracteristiques,
-          ...updateMap.caracteristiques
-        }
-      })
+      <BillingPanel billingState={state.billingState} />
+
+      <Caracteristiques caracteristiques={state.caracteristiques}
+        initialcaracteristiques={props.perso.caracteristiques}
+        onChangeCara={caraChangeHandler}
+        pa={state.pa}
+      />
+      <Status2 pa={state.pa} paTotal={state.paTotal} statusChangeHandler={statusChangeHandler}
+        force={state.caracteristiques.force} faction={state.faction} />
+
+    </Stack>
+  );
 
 
-    }; 
-
-    this.statusChangeHandler = (updatedState) =>{
-      this.setState(updatedState);
-    }
-  }
-  
-  render() {
-    return (
-      <Stack>
-
-        {/* <CreatureProportion size={3} weight={6} /> */}
-        <CaracteristiquesFunc caracteristiques={this.state.caracteristiques}
-        initialcaracteristiques={this.props.perso.caracteristiques}
-          onChangeCara = {this.onChangeCaraAndBilling}
-          pa={this.state.pa}
-        />
-        <Status2 pa={this.state.pa} paTotal={this.state.paTotal} statusChangeHandler={this.statusChangeHandler}
-        force={this.state.caracteristiques.force} faction={this.state.faction} />
-
-      </Stack>
-    );
-  }
 }
-
 function App() {
 
   let initialPerso = {
@@ -321,14 +325,14 @@ function App() {
     faction: FACTIONS.DEMONS,
     superieur: "Baal",
     grade: 3,
-  
+
     caracteristiques: {
       force: 4,
-    agilite: 6,
-    perception: 5,
-    volonte: 7,
-    presence: 1.5,
-    foi: 5,
+      agilite: 6,
+      perception: 5,
+      volonte: 7,
+      presence: 1.5,
+      foi: 5,
     },
     pa: 12,
     paTotal: 9001,
@@ -337,7 +341,7 @@ function App() {
   }
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Assistant3 perso={initialPerso} />
+      <FeuilleDePerso perso={initialPerso} />
     </MantineProvider>
   );
 }
